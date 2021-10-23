f0b229c058b434780a6d16d5e51c369b1a805ed1
TannerB <tannerpace@gmail.com>
2021-10-23 17:06:45 -0400

    adds sql script

---
+++ typer/sqlscript.sql
@@ -0,0 +1,28 @@
+drop schema if exists mytype;
+CREATE SCHEMA `mytype` 
+
+
+CREATE TABLE `mytype`.`users` (
+    id int unique not null auto_increment,
+    userName varchar(25) unique not null,
+    password varchar(255) not null,
+    dateJoined Datetime not null DEFAULT (current_date()),
+
+   
+    
+    primary key (id)
+);
+
+
+
+CREATE TABLE `mytype`.`games` (
+    id int unique not null auto_increment,
+    score varchar(140) not null,
+    userId int not null,
+    lang varchar(140),
+    dateAndTime datetime not null default now(),
+
+    primary key (id),
+    foreign key (userId) references users(id)
+    
+);
